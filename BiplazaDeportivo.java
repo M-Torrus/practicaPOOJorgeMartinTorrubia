d579c8b05366687e5e709850ed73675ed5a6d0aa
torrus <torrus@torrus>
2026-03-07 15:14:26 +0100

    revert cc bp

--- BiplazaDeportivo.java
+++ BiplazaDeportivo.java
@@ -1,6 +1,5 @@
 /**
- * Vehículo deportivo de dos plazas. El número de plazas es fijo (2)
- * y se establece automáticamente en el constructor.
+ * Vehículo deportivo de dos plazas.
  *
  * @author Jorge Martín Torrubia
  * @version 1.0
@@ -8,13 +7,14 @@
 public class BiplazaDeportivo extends Vehiculo {
 
     /**
-     * Constructor de BiplazaDeportivo. Las plazas quedan fijadas a 2.
+     * Constructor de BiplazaDeportivo.
      *
      * @param color             Color de la carrocería.
+     * @param numPlazas         Número de plazas del vehículo.
      * @param tara              Peso en vacío en kg.
      * @param pesoMaxAutorizado Peso máximo autorizado en kg.
      */
-    public BiplazaDeportivo(String color, double tara, double pesoMaxAutorizado) {
-        super(color, 2, tara, pesoMaxAutorizado);
+    public BiplazaDeportivo(String color, int numPlazas, double tara, double pesoMaxAutorizado) {
+        super(color, numPlazas, tara, pesoMaxAutorizado);
     }
 }
